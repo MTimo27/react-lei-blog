8f5716b9b19f9812217226c2e9d01a5169e5c3fa
Mihai Timoficiuc <mihaitimoficiuc@gmail.com>
2021-11-05 08:18:12 +0200

    Styled the about page more and started the projects page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -8,6 +8,81 @@ function About() {
       <section className="about">
         <div className="aboutContainer">
           <div className="aboutTitle">
+            <h1>Cine suntem?</h1>
+          </div>
+          <div className="aboutText">
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Eos eveniet, pariatur esse
+              dicta atque ad ex sapiente placeat, reiciendis
+              laudantium eligendi dignissimos quia deleniti,
+              perspiciatis veritatis iste dolore ipsam! Aut
+              sapiente laborum placeat tenetur ex
+              necessitatibus aspernatur quam officia culpa
+              accusantium at rem similique nam velit alias,
+              nobis enim, quidem delectus dolorem nostrum
+              et. Unde, ullam placeat deleniti ea iste saepe
+              consectetur voluptas tempora commodi
+              architecto numquam delectus officiis nostrum
+              vero nam nesciunt, voluptatum magnam impedit
+              dolore ipsa. Voluptatum alias sed eius
+              praesentium ex exercitationem illo vitae,
+              autem quia illum excepturi, ipsam minus
+              debitis, magni aliquam delectus neque vero
+              repellendus.
+            </p>
+          </div>
+          <div className="aboutTitle">
+            <h1>Misiune si obiective</h1>
+          </div>
+          <div className="aboutText">
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Eos eveniet, pariatur esse
+              dicta atque ad ex sapiente placeat, reiciendis
+              laudantium eligendi dignissimos quia deleniti,
+              perspiciatis veritatis iste dolore ipsam! Aut
+              sapiente laborum placeat tenetur ex
+              necessitatibus aspernatur quam officia culpa
+              accusantium at rem similique nam velit alias,
+              nobis enim, quidem delectus dolorem nostrum
+              et. Unde, ullam placeat deleniti ea iste saepe
+              consectetur voluptas tempora commodi
+              architecto numquam delectus officiis nostrum
+              vero nam nesciunt, voluptatum magnam impedit
+              dolore ipsa. Voluptatum alias sed eius
+              praesentium ex exercitationem illo vitae,
+              autem quia illum excepturi, ipsam minus
+              debitis, magni aliquam delectus neque vero
+              repellendus.
+            </p>
+          </div>
+          <div className="aboutTitle">
+            <h1>Principii</h1>
+          </div>
+          <div className="aboutText">
+            <p>
+              Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Eos eveniet, pariatur esse
+              dicta atque ad ex sapiente placeat, reiciendis
+              laudantium eligendi dignissimos quia deleniti,
+              perspiciatis veritatis iste dolore ipsam! Aut
+              sapiente laborum placeat tenetur ex
+              necessitatibus aspernatur quam officia culpa
+              accusantium at rem similique nam velit alias,
+              nobis enim, quidem delectus dolorem nostrum
+              et. Unde, ullam placeat deleniti ea iste saepe
+              consectetur voluptas tempora commodi
+              architecto numquam delectus officiis nostrum
+              vero nam nesciunt, voluptatum magnam impedit
+              dolore ipsa. Voluptatum alias sed eius
+              praesentium ex exercitationem illo vitae,
+              autem quia illum excepturi, ipsam minus
+              debitis, magni aliquam delectus neque vero
+              repellendus.
+            </p>
+          </div>
+          <div className="aboutTitle">
             <h1>Echipa</h1>
           </div>
           <div className="aboutAvatars">
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -2,7 +2,26 @@ import React from 'react';
 import './projects.css';
 
 function Projects() {
-  return <div>Projects page</div>;
+  return (
+    <>
+      <section className="projects">
+        <div className="projectsContainer">
+          <div className="projectsTitle">Proiecte</div>
+          <div className="projectsText">
+            Lorem ipsum dolor sit amet consectetur,
+            adipisicing elit. Dolorum, odit. Beatae cumque a
+            exercitationem necessitatibus veniam facilis
+            maiores, aut est officia quae voluptates ratione
+            accusantium, cupiditate obcaecati, consequuntur
+            eos expedita minima! Alias quos magnam excepturi
+            soluta tempore voluptates veritatis molestias
+            amet totam tenetur eius sint, quis perspiciatis
+            consequuntur! Harum, ullam.
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Projects;
