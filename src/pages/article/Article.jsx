a09d5f720f8089c0a69888b4654948120da6f07d
Mihai Timoficiuc <mihaitimoficiuc@gmail.com>
2021-11-05 21:30:41 +0200

    Styled the Article page

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,8 +1,70 @@
 import React from 'react';
 import './article.css';
+import Footer from '../../components/footer/Footer';
 
 function Article() {
-  return <div>Article page</div>;
+  return (
+    <>
+      <section className="article">
+        <div className="articleImg">
+          <img
+            src="https://scontent.fias1-1.fna.fbcdn.net/v/t39.30808-6/246622375_339500504613057_6302165414286729866_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=8bfeb9&_nc_ohc=jAmK10c2n7kAX-Py5sy&_nc_ht=scontent.fias1-1.fna&oh=28017c110fed829923f1973e4b48e2ad&oe=6187E572"
+            alt=""
+          />
+        </div>
+        <div className="articleContainer">
+          <div className="articleTitle">
+            <h1>
+              100 DE ANI DE LA NASTEREA REGELUI MIHAI I
+            </h1>
+          </div>
+          <div className="articleText">
+            12.10.2021 | Scris de: Username
+          </div>
+          <div className="articleText firstLetter">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Animi optio illo obcaecati,
+              fugiat, quod itaque minus voluptatem ad
+              reiciendis voluptate consectetur tempora
+              mollitia quas maiores pariatur vitae quis
+              laboriosam aspernatur laborum incidunt porro
+              eligendi quidem ipsam sequi. Mollitia non, id
+              autem officia culpa delectus perferendis
+              asperiores laudantium fuga laborum deleniti.
+            </p>
+          </div>
+          <div className="articleText">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Animi optio illo obcaecati,
+              fugiat, quod itaque minus voluptatem ad
+              reiciendis voluptate consectetur tempora
+              mollitia quas maiores pariatur vitae quis
+              laboriosam aspernatur laborum incidunt porro
+              eligendi quidem ipsam sequi. Mollitia non, id
+              autem officia culpa delectus perferendis
+              asperiores laudantium fuga laborum deleniti.
+            </p>
+          </div>
+          <div className="articleText">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur
+              adipisicing elit. Animi optio illo obcaecati,
+              fugiat, quod itaque minus voluptatem ad
+              reiciendis voluptate consectetur tempora
+              mollitia quas maiores pariatur vitae quis
+              laboriosam aspernatur laborum incidunt porro
+              eligendi quidem ipsam sequi. Mollitia non, id
+              autem officia culpa delectus perferendis
+              asperiores laudantium fuga laborum deleniti.
+            </p>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
 }
 
 export default Article;
